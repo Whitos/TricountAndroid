ed127769613a2c64da47ac9d729f660ca87104f9
Whitos <pavel.tsanev@lycee-faure.fr>
2024-10-09 11:16:08 +0200

    update

--- app/src/main/java/com/example/tricount/NewTricountActivity.java
+++ app/src/main/java/com/example/tricount/NewTricountActivity.java
@@ -1,6 +1,8 @@
 package com.example.tricount;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.tricount.databinding.ActivityNewTricountBinding;
+
 public class NewTricountActivity extends AppCompatActivity {
+
+    private ActivityNewTricountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_new_tricount);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityNewTricountBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.buttonCreerTricount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewTricountActivity.this, DepensesTricountActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
